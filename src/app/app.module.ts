b0282bbb05abbb0f9aa37573f328265dec653b23
jbdioli <jdeoworld@gmail.com>
2019-09-09 21:09:08 +0200

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,16 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
+import { CoreService } from './core/core.service';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SideMenuComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  exports: [SideMenuComponent],
   providers: [
+    CoreService,
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
---
+++ src/app/components/side-menu/side-menu.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuService } from 'src/app/services/menu.service';
+import { CoreService } from 'src/app/core/core.service';
+
+export interface MenuLink {
+  title: string;
+  linkHref: string;
+}
+
+@Component({
+  selector: 'app-side-menu',
+  templateUrl: './side-menu.component.html',
+  styleUrls: ['./side-menu.component.scss'],
+})
+export class SideMenuComponent implements OnInit {
+
+  constructor(public menu: MenuService, public core: CoreService) {}
+
+  ngOnInit() {}
+
+}
